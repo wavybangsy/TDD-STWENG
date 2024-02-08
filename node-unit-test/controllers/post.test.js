89a68d34886150cdc4cf010ebb27971fa816e3e1
wavybangsy <mary_erika_culala@dlsu.edu.ph>
2024-02-08 23:23:56 +0800

    Add failing test for Module A

--- node-unit-test/controllers/post.test.js
+++ node-unit-test/controllers/post.test.js
@@ -1,2 +1,33 @@
-import { addPost } from "./postController";
-import { jest } from "@jest/globals";
+const postController = require("./postController");
+const postModel = require("../models/post");
+
+// Mocking the getById and create functions of postModel
+jest.mock("../models/post", () => ({
+  getById: jest.fn(),
+  create: jest.fn(),
+}));
+
+describe("getPost function", () => {
+  it("should have error handling for post not found", () => {
+    const mockRequest = {
+      params: { id: "blahblah" },
+    };
+    const mockResponse = {
+      render: jest.fn(),
+    };
+
+    postModel.getById.mockImplementationOnce((id, callback) => {
+      callback(new Error("Post not found"), null);
+    });
+
+    postController.getPost(mockRequest, mockResponse);
+
+    expect(postModel.getById).toHaveBeenCalledWith(
+      "blahblah",
+      expect.any(Function)
+    );
+    expect(mockResponse.render).toHaveBeenCalledWith("error", {
+      errorMessage: "Post not found",
+    });
+  });
+});
